cef4a2735123284eb4a9b35fb0247827e1ea6222
Jakob <jakob-151@web.de>
2021-06-30 20:18:33 +0200

    fixed visuals by resorting party

--- web-python/static/javascripts/vis.js
+++ web-python/static/javascripts/vis.js
@@ -1,10 +1,10 @@
 // Frame for colors and names
 var data = [{ id: 1, party: 'Afd', passive_color: '#3D3BFB', active_color: '#1A18E0' },
 { id: 2, party: 'Union', passive_color: '#6F6F6F', active_color: '#151515' },
-{ id: 3, party: 'Gruene', passive_color: '#52B11F', active_color: '#4BD302' },
-{ id: 4, party: 'FDP', passive_color: '#F3FF56', active_color: '#FBFF00' },
-{ id: 5, party: 'SPD', passive_color: '#E93333', active_color: '#EB0101' },
-{ id: 6, party: 'LINKE', passive_color: '#9E49C0', active_color: '#8900C0' }]
+{ id: 3, party: 'FDP', passive_color: '#F3FF56', active_color: '#FBFF00' },
+{ id: 4, party: 'Gruene', passive_color: '#52B11F', active_color: '#4BD302' },
+{ id: 5, party: 'LINKE', passive_color: '#9E49C0', active_color: '#8900C0' },
+{ id: 6, party: 'SPD', passive_color: '#E93333', active_color: '#EB0101' }]
 
 var radius;
 
